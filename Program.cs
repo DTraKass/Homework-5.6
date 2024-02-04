5b290784a9de74e51d6bebda06ddd22725d6f444
demNo <Demonov03@gmail/com>
2024-02-04 12:11:46 +0300

    Req

--- Program.cs
+++ Program.cs
@@ -7,38 +7,41 @@ class Program
 		EnterUser();
 	}
 	static void EnterUser()
-    {
+	{
 		(string Name, string LastName, bool HasPet, string[] PetNames, string[] favcolors, double Age) User;
 
-		Console.WriteLine("¬ведите им€ пользовател€")
+		Console.WriteLine("¬ведите им€ пользовател€");
 		User.Name = Console.ReadLine();
 
-		Console.WriteLine("¬ведите фамилию пользовател€")
+		Console.WriteLine("¬ведите фамилию пользовател€");
 		User.LastName = Console.ReadLine();
 
 		string age;
 		int intAge;
 
-        do
-        {
-			Console.WriteLine("¬ведите возраст цифрами")
+		do
+		{
+			Console.WriteLine("¬ведите возраст цифрами");
 			age = Console.ReadLine();
 
-        } while(CheckNum(age, out intAge))
+		} while (CheckNum(age, out intAge));
 
 		User.Age = intAge;
+
 	}
 	static bool CheckNum(string number, out int corrNumber)
-    {
-		if(int.TryParse(number out int intNum){
-			if(intNum > 0)
-            {
+	{
+		if (int.TryParse(number, out int intNum))
+		{
+			if (intNum > 0)
+			{
 				corrNumber = intNum;
 				return false;
-            }else {
-				corrNumber = 0;
-				return true;
 			}
-        }
-    }
+		}
+		{
+			corrNumber = 0;
+			return true;
+		}
+	}
 }
