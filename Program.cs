5ff31ff6d636bd3264ff74377a4303b16b75f832
demNo <Demonov03@gmail/com>
2024-02-04 21:49:38 +0300

    ReqF

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ class Program
 	}
 	static void EnterUser()
 	{
-		(string Name, string LastName, bool HasPet, string[] PetNames, string[] favcolors, double Age) User;
+		(string Name, string LastName, bool HasPet, string[] PetNames, string[] FavColors, int Age) User;
+
+		User.HasPet = false;
 
 		Console.WriteLine("Введите имя пользователя");
 		User.Name = Console.ReadLine();
@@ -28,7 +30,42 @@ class Program
 
 		User.Age = intAge;
 
+		Console.WriteLine("Сколько у вас питомцев?");
+		int numPet = int.Parse(Console.ReadLine());
+		User.PetNames = new string[numPet];
+
+		if (numPet > 1)
+		{
+			User.HasPet = true;
+			Console.WriteLine("Здорово, назовете все клички питомцев?");
+
+			for (int i = 0; i < User.PetNames.Length; i++)
+			{
+				Console.WriteLine("Введите кличку питомца {0}", i + 1);
+				User.PetNames[i] = Console.ReadLine();
+			}
+		}
+		else if (numPet == 1)
+		{
+			User.HasPet = true;
+			Console.WriteLine("Здорово, назовете кличку питомца?");
+			User.PetNames[0] = Console.ReadLine();
+		}
+		else
+			Console.WriteLine("Очень жаль, вам стоит завести себе питомца)");
+
+		Console.WriteLine("Введите количество ваших любимых цветов");
+		User.FavColors = new string[int.Parse(Console.ReadLine())];
+
+		for (int i = 0; i < User.FavColors.Length; i++)
+		{
+			Console.WriteLine("Введите название любимого цвета {0}", i + 1);
+			User.FavColors[i] = Console.ReadLine();
+		}
+
+		UserData(User.Name, User.LastName, User.HasPet, User.PetNames, User.FavColors, User.Age);
 	}
+
 	static bool CheckNum(string number, out int corrNumber)
 	{
 		if (int.TryParse(number, out int intNum))
@@ -43,5 +80,28 @@ class Program
 			corrNumber = 0;
 			return true;
 		}
+	}
+
+	static void UserData(string Name, string LastName, bool HasPet, string[] PetNames, string[] FavColors, int Age)
+	{
+		Console.WriteLine($"Ваше имя: {Name}");
+		Console.WriteLine($"Ваша фамилия: {LastName}");
+		Console.WriteLine($"Ваш возраст: {Age}");
+
+		if (HasPet)
+		{
+			Console.WriteLine("Какие милые у вас питомцы:");
+			for (int i = 0; i < PetNames.Length; i++)
+			{
+				Console.WriteLine($"Питомец {i + 1}:\t {PetNames[i]}");
+			}
+		}
+
+		Console.WriteLine("Хмм, наши любимые цвета совпадают:");
+		for (int i = 0; i < FavColors.Length; i++)
+		{
+			Console.WriteLine($"Цвет {i + 1}:\t {FavColors[i]}");
+		}
+
 	}
 }
